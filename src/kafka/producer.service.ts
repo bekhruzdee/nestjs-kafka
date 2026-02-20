4b660d70e9498bf47d9e8220fc010c72f3d44e00
bekhruzdee <qutbidinovbehruz003@gmail.com>
2026-02-20 14:16:40 +0500

    updated

--- src/kafka/producer.service.ts
+++ src/kafka/producer.service.ts
@@ -1,28 +1,3 @@
-// import { Injectable, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
-// import { Kafka, Partitioners, Producer, ProducerRecord } from 'kafkajs';
-
-// // @Injectable()
-// // export class ProducerService implements OnModuleInit {
-// //   private readonly kafka = new Kafka({
-// //     brokers: ['localhost:9092'],
-// //   });
-
-// //   private readonly producer: Producer = this.kafka.producer();
-
-// //   async onModuleInit() {
-// //     await this.producer.connect();
-// //   }
-
-// //   async produce(record: ProducerRecord) {
-// //     await this.producer.send(record);
-// //   }
-
-// //   async onApplicationShutdown() {
-// //     await this.producer.disconnect();
-// //   }
-// // }
-
-// producer.service.ts
 import {
   Injectable,
   OnModuleInit,
